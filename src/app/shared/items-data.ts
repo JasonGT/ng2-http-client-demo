f0ab464ea55bb91e3d9e0b2e804755493e303a45
Jason Taylor <jasontaylor@ssw.com.au>
2016-10-21 15:33:37 +1000

    Clean-up

--- src/app/shared/items-data.ts
+++ src/app/shared/items-data.ts
@@ -6,10 +6,17 @@ export class ItemsData implements InMemoryDbService {
   createDb() {
     let items = [
       new Item(1, 'One bad programmer can easily create two new jobs a year'),
-      new Item(2, 'pro-gram-mer: n. an organism that turns caffine into software'),
-      new Item(3, 'My code is guaranteed 100% mistrake free.'),
-      new Item(4, '!false: it\'s funny because it\'s true.')
+      new Item(2, 'Pro-gram-mer: n. an organism that turns caffine into software'),
+      new Item(3, 'My code is guaranteed 100% mistrake free'),
+      new Item(4, '!false: it\'s funny because it\'s true')
     ];
+
+    // let items = [
+    //   new Item(1, 'This is an item.'),
+    //   new Item(2, 'This is another item.'),
+    //   new Item(3, 'This is yet another item.'),
+    //   new Item(4, 'You really should stop reading this list.')
+    // ];
 
     return {items};
   }
